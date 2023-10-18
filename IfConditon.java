a9940784c90d49266b5698685213b7331b3dcb37
Prabhu2625 <148323442+Prabhu2625@users.noreply.github.com>
2023-10-18 14:09:23 +0530

    Add files via upload
    Week1: Assignment:4 If Condition

---
+++ IfConditon.java
@@ -0,0 +1,13 @@
+package Week1.Day1;
+
+public class IfConditon {
+
+public static void main(String[] args) {
+	int a = -35;
+	if (a>9) {
+		System.out.println("The given number :" +a + "is Postive");
+	}else {
+		System.out.println("The given number :" +a + "is Negative");
+	}
+}
+}

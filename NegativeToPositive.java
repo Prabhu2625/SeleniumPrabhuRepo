4b40400454b7600a60c1d6cdd619b60f9d8b1643
Prabhu2625 <148323442+Prabhu2625@users.noreply.github.com>
2023-10-18 21:33:13 +0530

    Add files via upload

---
+++ NegativeToPositive.java
@@ -0,0 +1,13 @@
+package Week1.Day1;
+
+public class NegativeToPositive {
+
+	public static void main(String[] args) {
+		int b = -55;
+		if (b<0) {
+			
+			int output = b * -5;
+			System.out.println("The number is " +b +"is convert to " +output);
+		}
+	}
+}
